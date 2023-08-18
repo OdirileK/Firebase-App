cc2ed9a0738ad77aedb989ebbd9865f4a1a82334
OdirileK <emkayphozur@gmail.com>
2023-08-18 14:01:02 +0200

    deploy to firebase

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '16%'}}>
-<div >
-    <h1>This is my input</h1>
-    <input 
-    name='name'
-    type='text'
-    placeholder='Write something ...'
-    />
-    <button>Go</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "16%",
+      }}
+    >
+      <div>
+        <h1>This is my input</h1>
+        <input name="name" type="text" placeholder="Write something ..." />
+        <button>Go</button>
 
-    <h1>This is my second input</h1>
-    <input 
-    name='name'
-    type='text'
-    placeholder='Write something...'
-    />
-    <button>Go</button>
+        <h1>This is my second input</h1>
+        <input name="name" type="text" placeholder="Write something..." />
+        <button>Go</button>
 
-    <h1>This is my third input</h1>
-    <input 
-    name='name'
-    type='text'
-    placeholder='Write something...'
-    />
-    <button>Go</button>
+        <h1>This is my third input</h1>
+        <input name="name" type="text" placeholder="Write something..." />
+        <button>Go</button>
+      </div>
     </div>
-    </div>
-    
   );
 }
 
